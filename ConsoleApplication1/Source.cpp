eaa2c1e8f72ba5187b3298b7ea85ea1b0ebe9705
derofim <derofim@yandex.ru>
2015-08-08 00:17:27 +0300

    ss

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -1,44 +1,8 @@
 #include <iostream>
 using namespace std;
-class Base
-{
-    public:
-        Base() { std::cout << "Base constructor\n"; }
-        virtual void f() { std::cout << "Base f\n"; }
-        virtual ~Base() { std::cout << "Base destructor\n"; }
-};
-
-class Derived : public virtual Base
-{
-    public:
-        Derived() { std::cout << "Derived constructor\n"; }
-        void f() override { std::cout << "Derived f\n"; }
-        ~Derived() override { std::cout << "Derived destructor\n"; }
-};
-
-class Child : public virtual Base
-{
-    public:
-        Child() { std::cout << "Child constructor\n"; }
-        void f() override { std::cout << "Child f\n"; }
-        ~Child() override { std::cout << "Child destructor\n"; }
-};
 
 int main()
 {
-    {
-        std::cout << "===============\n";
-        Base* p = new Derived;
-        p->f();
-        delete p;
-        std::cout << "===============\n";
-    }
-    {
-        std::cout << "===============\n";
-        Base* p = new Child;
-        p->f();
-        delete p;
-        std::cout << "===============\n";
-    }
+    std::cout << "Hello world!";
     std::cin.get();
 }
